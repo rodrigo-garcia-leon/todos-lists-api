0d954b5ec88b89dfb19fa39f3e14eeee28ebb970
Rodrigo García <yo@rodrigogarcia.me>
2021-12-04 16:52:37 +0100

    Fix linting

--- src/app.py
+++ src/app.py
@@ -1,17 +1,18 @@
 from flask import Flask, jsonify
 from db import get_db
 
+
 def create_app():
     app = Flask(__name__)
 
     @app.route("/")
     def hello_world():
         return "Hello World"
-        
+
     @app.route("/todos")
     def todos():
         db = get_db()
-        todos = list(db.todos.find({}, { '_id': False }))
+        todos = list(db.todos.find({}, {'_id': False}))
 
         return jsonify(todos)
 
--- tests/test_app.py
+++ tests/test_app.py
@@ -4,12 +4,14 @@ from flask import g
 from app import create_app
 
 app = create_app()
-client = app.test_client();
+client = app.test_client()
+
 
 def test_hello_world():
     response = client.get('/')
 
     assert b'Hello World' in response.data
+
 
 class MockTodos:
     def find(*args, **kwargs):
@@ -19,9 +21,11 @@ class MockTodos:
             "done": False
         }]
 
+
 class MockDb:
     def __init__(self):
         self.todos = MockTodos()
+
 
 def test_todos():
     with app.app_context():
